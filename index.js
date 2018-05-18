e93db770dfb17f900018eee7bcfccfc4e60acf8c
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-18 20:52:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
+
+function 
